acbd57ad2585009475c110adbea98fe9fe6e28ce
JK624 <s006249@gmail.com>
2024-11-20 17:56:08 +0000

    feat: Add EDA(jisoo)

--- demo_streamlit/run.sh
+++ demo_streamlit/run.sh
@@ -1,1 +1,1 @@
-streamlit run app.py --global.developmentMode false --server.port=30534 --server.runOnSave=true
+streamlit run app.py --global.developmentMode false --server.port=30536 --server.runOnSave=true
