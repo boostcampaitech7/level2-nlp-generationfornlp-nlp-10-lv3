b8b6cb756cb4e2b8b675fe9eab3574a77e3803ed
LeeJeongHwi <wjdgnl97@gmail.com>
2024-11-25 17:06:49 +0000

    feat: Add insert, lagnchain Retrieval (not yet)

--- RAG/milvus_database.py
+++ RAG/milvus_database.py
@@ -1,7 +1,8 @@
-from pymilvus import MilvusClient
+from pymilvus import MilvusClient, FieldSchema, DataType, CollectionSchema
 from tqdm import tqdm
-
+import numpy as np 
 # Vector DB 구현
+
 class MilvusDatabase:
     def __init__(self, uri, collection_name, embedding_dim):
         self.milvus_client = MilvusClient(uri=uri) # milvus init
@@ -11,23 +12,45 @@ class MilvusDatabase:
     def set_collection(self):
         # Create Collection
         if not self.milvus_client.has_collection(self.collection_name):
+            schema = self.create_schema()
+            index_param = self.create_index()
+
             self.milvus_client.create_collection(
                 collection_name=self.collection_name,
-                dimension=self.embedding_dim, # embedding dimension
-                metric_type="COSINE", #innuer product
-                consistency_level="Strong"
+                schema=schema,
+                index_params=index_param
             )
+
+     # 스키마 생성
+    def create_schema(self):
+        fields = [
+            FieldSchema(name="id", dtype=DataType.INT64, is_primary=True, auto_id=True),
+            FieldSchema(name="text", dtype=DataType.VARCHAR, max_length=6000, description="raw Text"),
+            FieldSchema(name="embedding", dtype=DataType.FLOAT_VECTOR, dim=self.embedding_dim, description="vector")
+        ]
+        schema = CollectionSchema(fields=fields, auto_id=True, description="Rag schema")
+        return schema
+    
+    def create_index(self):
+        index_params = self.milvus_client.prepare_index_params()
+
+        index_params.add_index(
+            field_name="embedding",
+            index_type="FLAT",
+            metric_type="IP"
+        )
+        return index_params    
+    
 
     def insert_data(self, collection_name, datasets):
         # Insert Data to Collection
         
         for i in tqdm(range(len(datasets)), desc="Insert Data to Milvus DB"):
             row = datasets.iloc[i]
-
+            vector = np.array(row["emb"], dtype=np.float32)
             data = {
-                "id": row["id"],
                 "text": row["text"],
-                "vector": row["emb"],
+                "embedding": vector,
             }
             
             self.milvus_client.insert(collection_name=collection_name,
@@ -39,3 +62,6 @@ class MilvusDatabase:
         # Drop Collection
         if self.milvus_client.has_collection(self.collection_name):
             self.milvus_client.drop_collection(self.collection_name)
+
+    def describe(self):
+        return self.milvus_client.describe_collection(self.collection_name)
