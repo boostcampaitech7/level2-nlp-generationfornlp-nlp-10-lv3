3bac69ef662bf17a128aa5d019ce8e46fdb07e7c
kangjun205 <kangjun205@gmail.com>
2024-11-26 08:24:00 +0000

    feat: Add generation based inference with self consistency method #31

---
+++ generation_inference.py
@@ -0,0 +1,108 @@
+import os
+import re
+import argparse
+from collections import Counter
+
+import pandas as pd
+from langchain.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_huggingface import ChatHuggingFace, HuggingFacePipeline
+from tqdm import tqdm
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+
+from utils.utils import set_seed
+
+def main(arg):
+    ## parameters
+    SEED = arg.seed
+    MODEL_ID = arg.model_id
+    DATA_VER = arg.data_ver
+    K = arg.k
+
+    ## random seeding
+    set_seed(SEED)
+
+    ## data loading
+    BASE_DIR = os.getcwd()
+    DATA_DIR = os.path.join(BASE_DIR, "../../data", DATA_VER)
+    df = pd.read_csv(os.path.join(DATA_DIR, "train.csv"))
+    
+    ## prompt template loading
+    PROMPT_DIR = os.path.join(BASE_DIR, "prompts")
+    with open(os.path.join(PROMPT_DIR, "prompt_llama.txt"), "r") as f:
+        template = f.read()
+
+    ## model loading
+    tokenizer = AutoTokenizer.from_pretrained(MODEL_ID)
+    model = AutoModelForCausalLM.from_pretrained(MODEL_ID)
+
+    ## pipline constructing
+    pipe = pipeline(
+        "text-generation",
+        model=model,
+        tokenizer=tokenizer,
+        max_new_tokens=256,
+        device=0,
+        batch_size=1
+    )
+    llm = HuggingFacePipeline(pipeline=pipe)
+    llm = ChatHuggingFace(llm=llm)
+
+    prompt = PromptTemplate.from_template(template)
+    chain = prompt | llm.bind(stop=[r"\n"]) | StrOutputParser()
+
+    ## inference
+    outputs = []
+    rep_out = []
+    for _, row in tqdm(df.iterrows()):
+        for _ in range(K):
+            answer = chain.invoke(
+                {
+                    "paragraph": row.paragraph,
+                    "question": row.question,
+                    "choices":  row.choices,
+                }
+            )
+
+            ## answer extraction in prompt
+            answer = answer.split("\n").pop()
+            rep_out.append(answer) ## parsing하고 넣어야 함
+
+        majority = Counter(rep_out)
+        majority = list(sorted(majority.item(), key=lambda x: x[1]))
+        majority = majority.pop()[0]
+        outputs.append(majority)
+
+if __name__ == "__main__":
+    args = argparse.ArgumentParser()
+    args.add_argument(
+        "-s",
+        "--seed",
+        default=456,
+        type=int,
+        help="setting random seed (default: 456)",
+    )
+    args.add_argument(
+        "-m",
+        "--model_id",
+        default=None,
+        type=str,
+        help="hugging face model id (default: None)",
+    )
+    args.add_argument(
+        "-v",
+        "--data_ver",
+        default=1.0,
+        type=str,
+        help="dataset version (default: 1.0)",
+    )
+    args.add_argument(
+        "-k",
+        "--k",
+        default=5,
+        type=int,
+        help="the number of repeatition for self-consistency prompting (default: 5)",
+    )
+
+    arg = args.parse_args()
+    main(arg)
